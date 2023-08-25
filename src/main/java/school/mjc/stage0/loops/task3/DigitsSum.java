49f9e124e809a40a45e97977c9c6a9e5a57a5b66
Simonas Zilevicius <s.zilevicius@gmail.com>
2023-08-25 15:31:32 +0300

    Task solved

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+        String str = Integer.toString(Math.abs(t));
+        int answer = 0;
+        for (int i = 0; i < str.length(); i++) {
+            answer += Integer.parseInt(String.valueOf(str.charAt(i)));
+        } System.out.println(answer);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
+        String nine = "9";
+        int answer = 0;
+        for (int i = 1; i <= lengthOfLastNumber; i++) {
+            answer += Integer.parseInt(nine);
+            nine += "9";
 
+        } System.out.println(answer);
     }
 }
